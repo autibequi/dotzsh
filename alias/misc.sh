9eb66773cb79ba7ddad916cea03fe9bad19bd68f
Pedro Correa <pedro.correa@concrete.com.br>
2018-05-13 00:13:42 -0300

    Adds a failover to ls custom alias.

--- alias/misc.sh
+++ alias/misc.sh
@@ -1,5 +1,5 @@
 alias cls="colorls -lA --sd"
-alias ls="exa"
+alias ls="exa || ls"
 alias fzy="fzy -l 20"
 alias sch="history | cut -c 8- | fzy -s -p 'COMMAND HISTORY SEARCH > '"
 alias cat='pygmentize -g'
