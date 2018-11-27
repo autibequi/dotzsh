729f27b53a96ede07b6b0ce047515ed5e7db90c8
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2018-11-27 11:20:48 -0200

    Fix exa and colorls alias

--- alias/misc.sh
+++ alias/misc.sh
@@ -1,5 +1,5 @@
-alias cls="colorls -lA --sd"
-alias ls="exa || ls"
+alias cls="colorls"
+alias ls="exa"
 alias fzy="fzy -l 20"
 alias sch="history | cut -c 8- | fzy -s -p 'COMMAND HISTORY SEARCH > '"
 alias cat='pygmentize -g'
