8091097db44f6011056c22bf1ed9c400b33c7d6a
Pedro Correa <thanos@infinity.com>
2018-11-29 00:58:33 -0200

    Unify dependencies on nix

--- nix.sh
+++ nix.sh
@@ -7,7 +7,18 @@ nix-env -iA nixpkgs.alacritty
 # Install Tmux
 nix-env -iA nixpkgs.tmux
 
+# Install python and ruby
+nix-env -iA nixpkgs.ruby
+nix-env -iA nixpkgs.python
+
 # Install Utils
-nix-env -iA nixpkgs.exa
-nix-env -iA nixpkgs.tig
-nix-env -iA nixpkgs.jq
+nix-env -iA nixpkgs.exa         # color ls
+nix-env -iA nixpkgs.tig         # gui git
+nix-env -iA nixpkgs.jq          # json grabber
+
+# Install Python Utils
+pip install howdoi              # code Cheat Sheets
+pip install Pygments            # color Cat
+
+# Install Ruby Utils
+gem install colorls             # pretty ls with icons
