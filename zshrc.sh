337c443c4f492b8c87365296b4a2781d5d93c1b7
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2018-12-03 23:44:33 -0200

    300% speed improvement :)

--- zshrc.sh
+++ zshrc.sh
@@ -35,15 +35,17 @@ zgen () {
 
 if [[ ! -s ${ZDOTDIR:-${HOME}}/.zgen/init.zsh ]]; then
 
-  # oh-my-zsh
-  zgen oh-my-zsh
-  zgen oh-my-zsh plugins/git
-  zgen oh-my-zsh plugins/colorize
+  # Mostly Shell
+  zgen load Tarrasch/zsh-bd
+  zgen load rupa/z
+  zgen load junegunn/fzf-bin
+  zgen load jhawthorn/fzy
+  zgen load raylee/tldr
+  zgen load molovo/tipz
 
   # github
   zgen load zdharma/fast-syntax-highlighting
   zgen load zsh-users/zsh-history-substring-search
-
   zgen load zsh-users/zsh-autosuggestions
   zgen load zsh-users/zsh-completions src
   zgen load b4b4r07/zsh-vimode-visual
@@ -59,13 +61,12 @@ if [[ ! -s ${ZDOTDIR:-${HOME}}/.zgen/init.zsh ]]; then
   zcompile ${ZDOTDIR:-${HOME}}/.zgen/init.zsh
 fi
 
-# Theme
-autoload -Uz promptinit && promptinit
-prompt purer             
-PS1=$'\n\n'"$PS1"                        # Adds an empty space between commands
+# Load settings
+source ${ZDOTDIR:-${HOME}}/.zgen/init.zsh
 
-# Manually source Async Plugin 
-source ${ZDOTDIR:-${HOME}}/.zgen/mafredri/zsh-async-master/async.plugin.zsh
+# Theme
+autoload -Uz promptinit && promptinit && prompt purer  
+PS1=$'\n\n'"$PS1"  # Adds an empty space between commands
 
 #     _    ______   ___   _  ____ 
 #    / \  / ___\ \ / / \ | |/ ___|
@@ -73,6 +74,13 @@ source ${ZDOTDIR:-${HOME}}/.zgen/mafredri/zsh-async-master/async.plugin.zsh
 #  / ___ \ ___) || | | |\  | |___ 
 # /_/   \_\____/ |_| |_| \_|\____|
 async_load() {
+  # Import/Create .localrc file
+  source ~/.localrc || touch ~/.localrc
+  
+  # Load Scripts
+  for f in ~/zsh/scripts/*.sh; do source $f; done
+  for f in ~/zsh/alias/*.sh; do source $f; done
+
   # Superdupper hack to speed up startup
   # https://carlosbecker.com/posts/speeding-up-zsh/
   autoload -Uz compinit
@@ -81,21 +89,6 @@ async_load() {
   else
     compinit -C
   fi
-
-  # Import/Create .localrc file
-  source ~/.localrc || touch ~/.localrc
-  
-  # Mostly Shell
-  zgen load Tarrasch/zsh-bd
-  zgen load rupa/z
-  zgen load junegunn/fzf-bin
-  zgen load jhawthorn/fzy
-  zgen load raylee/tldr
-  zgen load molovo/tipz
-
-  # Load Scripts
-  for f in ~/zsh/scripts/*.sh; do source $f; done
-  for f in ~/zsh/alias/*.sh; do source $f; done
 }
 
 # Initialize a new worker (with notify option)
